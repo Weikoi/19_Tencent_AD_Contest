de1a0f08ce3a147d44ae52f3ca517ac443a88771
Weikoi <huangzengrui@yahoo.com>
2019-05-08 22:21:47 +0800

    init proj

---
+++ src/fake_result/demo.py
@@ -0,0 +1,12 @@
+# 1-20290
+# 20-250
+
+import pandas as pd
+import numpy as np
+import random
+
+df = pd.DataFrame(np.random.rand(20290, 2))
+df[0] = [i for i in range(1, 20291)]
+df[1] = [round(random.uniform(20, 250), 4) for i in range(1, 20291)]
+print(df)
+df.to_csv("submission.csv", index=False, header=False)
---
+++ util/global_constant.py
@@ -0,0 +1,1 @@
+RAW_DATA_PATH = "D:/PycharmProjects/19_s1/19_Tencent_AD_Contest/data/raw/testA/"
