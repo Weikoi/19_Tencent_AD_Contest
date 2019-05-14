bbdfb8244119b690727ea67d8714437d2f573312
Weikoi <huangzengrui@yahoo.com>
2019-05-14 09:30:57 +0800

    reference

---
+++ data/raw/testA/refer/2_1_user_data_clean.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/4/30 14:40
+# @Author  : YYLin
+# @Email   : 854280599@qq.com
+# @File    : Dataload_User.py
+import pandas as pd
+
+userFeature_data = []
+
+user_Feature_columns = ['user_id', 'Age', 'Gender', 'Area', 'Marriage_Status', 'Education', 'Consuption_Ability',
+                        'Device',
+                        'Work_Status', 'Connection_Type', 'Behavior']
+
+# 为数据集增加列名称 对于userFeature_data不需要对某一列的数据进行处理
+userFeature_data.append(user_Feature_columns)
+with open('../user_data', 'r') as f:
+    for i, line in enumerate(f):
+        # print(i, ':', line,'\n', len(line), type(line))
+        line = line.strip().split('\t')
+        # print(i, ':', line, type(line),'\n', len(line))
+        # if i > 1000:
+        # break
+        userFeature_data.append(line)
+
+print("***********userFeature_data[0]:\n", userFeature_data[1])
+print("***********userFeature_data[0][1]:\n", userFeature_data[1][0])
+user_feature = pd.DataFrame(userFeature_data)
+print("***********正在保存数据集************")
+user_feature.to_csv('../Dataset/dataset_for_train/userFeature.csv', index=False, header=False)
+
